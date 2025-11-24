7ee1c262b07a64c0d34ea546de52343d031c82d6
oshanYelena <oshanranasinghe25@gmail.com>
2025-11-25 05:06:57 +0530

    bookfair-backend: stall repository updated

--- bookfair-backend/src/main/java/com/bookfair/repository/StallRepository.java
+++ bookfair-backend/src/main/java/com/bookfair/repository/StallRepository.java
@@ -3,8 +3,15 @@ package com.bookfair.repository;
 import com.bookfair.entity.Stall;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StallRepository extends JpaRepository<Stall, String> {
     long countByStatus(String status);
     long countBySize(String size);
     long countBySizeAndStatus(String size, String status);
+    boolean existsByName(String name);
+    Optional<Stall> findByName(String name);
+
+    boolean existsById(String id);
+
 }
